f1b674443dcace571cbe87eb0b3372377b7325af
Varinder Dhillon <98728916+Varinder-Dhillon0@users.noreply.github.com>
2024-01-16 19:03:10 +0530

    gitignore updated

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,18 +3,7 @@ import * as firebase from "firebase/app";
 import {getAuth} from "firebase/auth";  //this is users data getting system
 import {getFirestore} from "firebase/firestore";  //this is for database 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-
-  // A FILE NAMES ENV.LOCAL IS MADE AND ALL VARIABLES MAIN VALUE IS STORED THERE
-  // HERE WE GIVEN THOSE VARIABLES NAME WITH PROCESS.ENV
-  // THIS IS TO PREVENT USERS FROM GETTING OUR DATABASE INFO
-  // AND PREVENTING THEM FROM RETREIVING INFO
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
